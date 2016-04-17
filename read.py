1e0fb643c340cd6ecb8a6967ed2a830def79ce8e
pakhandi <asimkprasad@gmail.com>
2016-04-17 22:36:05 +0530

    Add colorMatrix

--- read.py
+++ read.py
@@ -3,6 +3,9 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+OFFSET = 5
+LIMIT = 3
+
 LeftUpX = 150
 LeftUpY = 100
 
@@ -14,6 +17,7 @@ RightDownX = 420
 RightDownY = 370
 
 pointsMatrix = [[(0,0) for i in xrange(0,4)] for i in xrange(0,4)]
+colorMatrix = [[[0,0,0] for i in xrange(0,4)] for i in xrange(0,4)]
 
 pointsMatrix[0][0] = (LeftUpX,LeftUpY)
 
@@ -41,55 +45,41 @@ while(True):
 
 	cv2.imshow('frame', frame)
 
-	for i in xrange(3):
-		for j in xrange(3):
-			img = frame[pointsMatrix[i][j][1]:pointsMatrix[i + 1][j + 1][1] , pointsMatrix[i][j][0]:pointsMatrix[i + 1][j + 1][0] ]
-			cv2.imwrite("images/img"+str(i)+str(j)+".jpg", img)
-
 	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+		for i in xrange(3):
+			for j in xrange(3):
+				img = frame[pointsMatrix[i][j][1]:pointsMatrix[i + 1][j + 1][1] , pointsMatrix[i][j][0]:pointsMatrix[i + 1][j + 1][0] ]
+				cv2.imwrite("images/img"+str(i)+str(j)+".jpg", img)
+				height, width, depth = img.shape
 
+				hc = height / 2
+				wc = width / 2
+
+				YUpper = hc + OFFSET
+				YLower = hc - OFFSET
+
+				XUpper = wc + OFFSET
+				XLower = wc - OFFSET
+
+				color = [0 for loop1 in xrange(3)]
+
+				for loop1 in range(XLower, XUpper):
+					for loop2 in range(YLower, YUpper):
+						for loop3 in xrange(3):
+							color[loop3] += img[loop1, loop2, loop3]
+
+				for loop1 in xrange(3):
+					color[loop1] /= ( (2 * OFFSET) ** 2 ) * 1.0
+
+				colorMatrix[i][j] = color
+
+		break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
 
-color0 = 0
-color1 = 0
-color2 = 0
-img0 = cv2.imread('images/img00.jpg')
-height, width, depth = img0.shape
-
-hc = height / 2
-wc = width / 2
-
-YUpper = hc + 5
-YLower = hc - 5
-
-XUpper = wc + 5
-XLower = wc - 5
-
-# print XLower, " ",XUpper, " ", YLower, " ", YUpper
-
-
-# print height
-# print width
-# print depth
-# print img0.size
-for i in range(XLower, XUpper):
-	for j in range(YLower, YUpper):
-		# print i," ",j
-		color0 += img0[i, j, 0]
-		color1 += img0[i, j, 1]
-		color2 += img0[i, j, 2]
-
-color0 /= (100)
-color1 /= (100)
-color2 /= (100)
-
-# print img0[10, 10, 0]
-# print img0[10, 10, 1]
-# print img0[10, 10, 2]
-print color0
-print color1
-print color2
+for loop1 in xrange(LIMIT):
+	for loop2 in xrange(LIMIT):
+		print colorMatrix[loop1][loop2]," ",
+	print ""
